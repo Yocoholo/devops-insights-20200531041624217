eac69ce42c97177975afada1ee5bc85766445727
Reef <reef-proctor@hotmail.co.nz>
2020-06-04 18:43:55 +1200

    2nd requirement fully satisfied

--- public/Map.js
+++ public/Map.js
@@ -4,7 +4,6 @@ var BULLS = {
     lat: -40.174,
     lng: 175.384
 }
-//-40.174,175.384
 
 function initMap() {
     map = new google.maps.Map(document.getElementById('map'), {
@@ -17,26 +16,30 @@ function initMap() {
         animation: google.maps.Animation.DROP,
         position: BULLS
     });
-    marker.addListener('dragend', toggleBounce);
-}
-
-function toggleBounce() {
-    marker.setAnimation(google.maps.Animation.BOUNCE);
-    var json = fetch(`https://api.opencagedata.com/geocode/v1/json?q=${marker.position.lat()}+${marker.position.lng()}&key=4c0b87285a1d43c09119fa726366c65e`).then(function(response){
-        return response.json();
-    }).then(function(obj){
-        console.log(obj)
-        let components = obj.results[0].components
-        console.log(components)
-        var value;
-        if(components.town != undefined) value = components.town
-        else if(components.county != undefined) value = components.county
-        else if(components.city != undefined) value = components.city
-        var textBox = document.getElementById("textbox");
-        textBox.setAttribute("value", value)
-    }).catch(function(error){
-        console.error('something went wrong when retrieving the json')
-        console.error(error)
-    })
-    marker.setAnimation(null);
+    map.addListener('click', function (mapsMouseEvent) {
+        console.log(mapsMouseEvent.latLng.toString())
+        marker.setPosition(mapsMouseEvent.latLng)
+        fetch(`https://api.opencagedata.com/geocode/v1/json?q=${marker.position.lat()}+${marker.position.lng()}&key=4c0b87285a1d43c09119fa726366c65e`).then(function (response) {
+            return response.json();
+        }).then(function (obj) {
+            console.log(obj)
+            let components = obj.results[0].components
+            console.log(components)
+            var val;
+            if (components.city != undefined) val = components.city
+            else if (components.town != undefined) val = components.town
+            else if (components.county != undefined) val = components.county
+            console.log(val)
+            var textbox = document.getElementById("textboxSearchBar").value = val;
+            document.getElementById("textboxSearchBar").click();
+            
+        }).catch(function (error) {
+            console.error('something went wrong when retrieving the json')
+            console.error(error)
+        })
+    });
+    marker.addListener('click', function () {
+        map.setZoom(map.zoom + 1);
+        map.setCenter(marker.getPosition());
+    });
 }
