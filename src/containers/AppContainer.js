76114d01aa8495c3913b91861a000c2021863be9
Reef <reef-proctor@hotmail.co.nz>
2020-06-05 13:42:10 +1200

    Renaming all zips to city

--- src/containers/AppContainer.js
+++ src/containers/AppContainer.js
@@ -8,7 +8,6 @@ function AppContainer(props) {
     const [responseData, setResponseData] = useState('');
 
     const handleCityNameChange = async (cityName) => {
-        //console.log(`--------- fetchData called zip:${zipValue}`)
         const res = await fetch(`https://api.openweathermap.org/data/2.5/weather?appid=6b7b471967dd0851d0010cdecf28f829&units=metric&q=${cityName},nz`)
         const json = await res.json()
         //console.log(json);
